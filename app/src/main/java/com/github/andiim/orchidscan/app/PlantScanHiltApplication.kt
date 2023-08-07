5309a83f5c53e435845c07a2ecd27be9806aad7a
Andi-IM <andi.irhamm@gmail.com>
2023-08-07 19:22:20 +0700

    fix : resolve hilt error

--- app/src/main/java/com/github/andiim/orchidscan/app/PlantScanHiltApplication.kt
+++ app/src/main/java/com/github/andiim/orchidscan/app/PlantScanHiltApplication.kt
@@ -2,8 +2,10 @@ package com.github.andiim.orchidscan.app
 
 import android.app.Application
 import com.github.andiim.orchidscan.app.di.DebugModule.provideTimberTree
+import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
+@HiltAndroidApp
 class PlantScanHiltApplication : Application() {
     override fun onCreate() {
         super.onCreate()
