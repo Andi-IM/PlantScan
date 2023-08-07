0c6dd38ebf2ef40dbd13d849c0da289756008d73
Andi-IM <andi.irhamm@gmail.com>
2023-08-07 11:17:57 +0700

    refactor : connect viewModel into detect screen

--- app/src/main/java/com/github/andiim/orchidscan/app/ui/screens/detect/DetectScreen.kt
+++ app/src/main/java/com/github/andiim/orchidscan/app/ui/screens/detect/DetectScreen.kt
@@ -3,10 +3,14 @@ package com.github.andiim.orchidscan.app.ui.screens.detect
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.github.andiim.orchidscan.app.ui.theme.PlantScanTheme
 
 @Composable
-fun DetectScreen() {
+fun DetectScreen(
+    popUpScreen: () -> Unit,
+    viewModel: DetectViewModel = hiltViewModel()
+) {
     DetectContent()
 }
 
