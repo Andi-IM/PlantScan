94d8fc89858f24a47226bcd87f59e33ae04e27f5
Andi-IM <andi.irhamm@gmail.com>
2023-08-07 19:10:31 +0700

    fix : forgot to implementation hilt in activity

--- app/src/main/java/com/github/andiim/orchidscan/app/PlantScanActivity.kt
+++ app/src/main/java/com/github/andiim/orchidscan/app/PlantScanActivity.kt
@@ -3,7 +3,9 @@ package com.github.andiim.orchidscan.app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class PlantScanActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
