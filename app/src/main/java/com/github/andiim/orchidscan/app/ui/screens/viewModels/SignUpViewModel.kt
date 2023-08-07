d01da51acd2ac5bc96737d0b53b45c5b58e4b42a
Andi-IM <andi.irhamm@gmail.com>
2023-08-07 10:01:31 +0700

    refactor : implement signUp Screen

--- app/src/main/java/com/github/andiim/orchidscan/app/ui/screens/viewModels/SignUpViewModel.kt
+++ app/src/main/java/com/github/andiim/orchidscan/app/ui/screens/viewModels/SignUpViewModel.kt
@@ -1,4 +1,58 @@
 package com.github.andiim.orchidscan.app.ui.screens.viewModels
 
-class SignUpViewModel {
+import com.github.andiim.orchidscan.app.R
+import com.github.andiim.orchidscan.app.data.firebase.AccountService
+import com.github.andiim.orchidscan.app.data.firebase.LogService
+import com.github.andiim.orchidscan.app.ui.common.snackbar.SnackbarManager
+import com.github.andiim.orchidscan.app.ui.screens.auth.signUp.SignUpState
+import com.github.andiim.orchidscan.library.android.extensions.isValidEmail
+import com.github.andiim.orchidscan.library.android.extensions.isValidPassword
+import com.github.andiim.orchidscan.library.android.extensions.passwordMatches
+import javax.inject.Inject
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class SignUpViewModel @Inject
+constructor(private val accountService: AccountService, logService: LogService) :
+    PlantScanViewModel(logService) {
+
+    private val _state = MutableStateFlow(SignUpState())
+    val state = _state.asStateFlow()
+
+    private val email = _state.value.email
+    private val password = _state.value.password
+
+    fun onEmailChange(newValue: String) {
+        _state.value = _state.value.copy(email = newValue)
+    }
+
+    fun onPasswordChange(newValue: String) {
+        _state.value = _state.value.copy(password = newValue)
+    }
+
+    fun onRepeatPasswordChange(newValue: String) {
+        _state.value = _state.value.copy(repeatPassword = newValue)
+    }
+
+    fun onSignUpClick(openAndPopUp: (String, String) -> Unit) {
+        if (!email.isValidEmail()) {
+            SnackbarManager.showMessage(R.string.email_error)
+            return
+        }
+
+        if (!password.isValidPassword()) {
+            SnackbarManager.showMessage(R.string.error_wrong_password)
+            return
+        }
+
+        if (!password.passwordMatches(_state.value.repeatPassword)) {
+            SnackbarManager.showMessage(R.string.error_password_match)
+            return
+        }
+
+        launchCatching {
+            accountService.linkAccount(email, password)
+            openAndPopUp("mandatory", "mandatory")
+        }
+    }
 }
