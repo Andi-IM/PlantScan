7d8cca279421aa3063cacfc40fd663962dc465ce
Andi-IM <andi.irhamm@gmail.com>
2023-08-07 11:19:01 +0700

    refactor : connect viewModel into find plant element

--- app/src/main/java/com/github/andiim/orchidscan/app/ui/screens/home/findPlant/FindPlantElement.kt
+++ app/src/main/java/com/github/andiim/orchidscan/app/ui/screens/home/findPlant/FindPlantElement.kt
@@ -2,17 +2,25 @@ package com.github.andiim.orchidscan.app.ui.screens.home.findPlant
 
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.github.andiim.orchidscan.app.ui.screens.auth.signUp.SignUpContent
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.github.andiim.orchidscan.app.data.model.Plant
 import com.github.andiim.orchidscan.app.ui.theme.PlantScanTheme
 
 @Composable
-fun FindPlantElement(){
-
+fun FindPlantElement(
+    modifier: Modifier = Modifier,
+    onDetails: (Plant) -> Unit,
+    toDetect: () -> Unit,
+    toPlantType: () -> Unit,
+    viewModel: FindPlantViewModel = hiltViewModel()
+) {
+    FindPlantContent()
 }
 
 @Composable
-fun FindPlantContent(){
+fun FindPlantContent() {
 
 }
 
--- app/src/main/java/com/github/andiim/orchidscan/app/ui/screens/home/findPlant/FindPlantViewModel.kt
+++ app/src/main/java/com/github/andiim/orchidscan/app/ui/screens/home/findPlant/FindPlantViewModel.kt
@@ -1,4 +1,11 @@
 package com.github.andiim.orchidscan.app.ui.screens.home.findPlant
 
-class FindPlantViewModel {
+import com.github.andiim.orchidscan.app.data.firebase.LogService
+import com.github.andiim.orchidscan.app.ui.screens.viewModels.PlantScanViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class FindPlantViewModel @Inject constructor(val logService: LogService) :
+    PlantScanViewModel(logService) {
 }
