0259ce36f9259e17d128e4b65f4aa913f11c792a
Andi-IM <andi.irhamm@gmail.com>
2023-08-13 22:39:50 +0700

    refactor: separating module (make service module instead take in one app module)

--- app/src/main/java/com/github/andiim/plantscan/app/di/AppModule.kt
+++ app/src/main/java/com/github/andiim/plantscan/app/di/AppModule.kt
@@ -1,11 +1,5 @@
 package com.github.andiim.plantscan.app.di
 
-import com.github.andiim.plantscan.app.core.domain.usecase.firebase_services.AccountService
-import com.github.andiim.plantscan.app.core.domain.usecase.firebase_services.ConfigurationService
-import com.github.andiim.plantscan.app.core.domain.usecase.firebase_services.LogService
-import com.github.andiim.plantscan.app.core.data.source.firebase.FakeAccountServiceImpl
-import com.github.andiim.plantscan.app.core.data.source.firebase.FakeConfigurationServiceImpl
-import com.github.andiim.plantscan.app.core.data.source.firebase.FakeLogServiceImpl
 import com.github.andiim.plantscan.app.core.domain.usecase.PlantInteractor
 import com.github.andiim.plantscan.app.core.domain.usecase.PlantUseCase
 import dagger.Binds
@@ -18,16 +12,4 @@ import dagger.hilt.android.scopes.ViewModelScoped
 @InstallIn(ViewModelComponent::class)
 abstract class AppModule {
   @Binds @ViewModelScoped abstract fun provideUseCase(interactor: PlantInteractor): PlantUseCase
-
-  @Binds
-  @ViewModelScoped
-  abstract fun provideAccountService(service: FakeAccountServiceImpl): AccountService
-
-  @Binds @ViewModelScoped abstract fun provideLogService(service: FakeLogServiceImpl): LogService
-
-  @Binds
-  @ViewModelScoped
-  abstract fun provideConfigurationService(
-      service: FakeConfigurationServiceImpl
-  ): ConfigurationService
 }
