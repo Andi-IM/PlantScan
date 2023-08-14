02df1b224485247cce314ac5574e5a47c49bee5e
Andi-IM <andi.irhamm@gmail.com>
2023-08-14 10:22:07 +0700

    test: (never mind) try to create end-to-end test with compose ui

--- app/src/androidTest/kotlin/com/github/andiim/plantscan/app/PlantScanAppTest.kt
+++ app/src/androidTest/kotlin/com/github/andiim/plantscan/app/PlantScanAppTest.kt
@@ -1,45 +1,40 @@
 package com.github.andiim.plantscan.app
 
-import androidx.activity.compose.setContent
-import androidx.activity.viewModels
-import androidx.compose.ui.test.assertIsDisplayed
-import androidx.compose.ui.test.junit4.createAndroidComposeRule
-import androidx.compose.ui.test.onNodeWithText
-import com.github.andiim.plantscan.app.ui.screens.list.PlantListScreen
-import com.github.andiim.plantscan.app.ui.screens.list.PlantListViewModel
-import com.github.andiim.plantscan.app.ui.theme.PlantScanTheme
+import androidx.test.espresso.IdlingRegistry
+import androidx.test.espresso.intent.Intents
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import com.github.andiim.plantscan.app.utils.EspressoIdlingResource
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
 
+@RunWith(AndroidJUnit4::class)
+@LargeTest
 @HiltAndroidTest
 class PlantScanAppTest {
 
   @get:Rule(order = 1) val hilt = HiltAndroidRule(this)
-
-  @get:Rule(order = 2) val composeRule = createAndroidComposeRule<PlantScanActivity>()
-  private lateinit var viewModel: PlantListViewModel
+  @get:Rule(order = 2) val activity = ActivityScenarioRule(PlantScanActivity::class.java)
 
   @Before
   fun setUp() {
     hilt.inject()
-    composeRule.activity.setContent {
-      viewModel = composeRule.activity.viewModels<PlantListViewModel>().value
-      PlantScanTheme {
-        PlantListScreen(
-            toDetails = { _,
-              ->
-            },
-            popUpScreen = {},
-            viewModel = viewModel)
-      }
-    }
+    IdlingRegistry.getInstance().register(EspressoIdlingResource.countingIdlingResource)
+  }
+
+  @After
+  fun tearDown() {
+    IdlingRegistry.getInstance().unregister(EspressoIdlingResource.countingIdlingResource)
   }
 
   @Test
-  fun testing() {
-    composeRule.onNodeWithText("Plants").assertIsDisplayed()
+  fun navHost_verifyStartDestination() {
+    Intents.init()
   }
 }
