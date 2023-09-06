70a5ccbf8db04f1e0fae800ff6b6a78d7b331805
Andi-IM <andi.irhamm@gmail.com>
2023-09-06 20:10:35 +0700

    implement webviewmodel

--- app/src/main/java/com/github/andiim/plantscan/app/ui/screens/web/WebViewModel.kt
+++ app/src/main/java/com/github/andiim/plantscan/app/ui/screens/web/WebViewModel.kt
@@ -1,18 +1,14 @@
 package com.github.andiim.plantscan.app.ui.screens.web
 
 import androidx.lifecycle.SavedStateHandle
-import com.github.andiim.plantscan.app.PlantScanViewModel
-import com.github.andiim.plantscan.app.core.domain.usecase.PlantUseCase
-import com.github.andiim.plantscan.app.core.domain.usecase.firebase_services.LogService
+import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class WebViewModel @Inject constructor(
-    useCase: PlantUseCase,
     savedStateHandle: SavedStateHandle,
-    logService: LogService,
-) : PlantScanViewModel(logService) {
-    // private val webArgs: WebArgs = WebArgs(savedStateHandle)
-    // val webUrl = webArgs.webUrl
+) : ViewModel() {
+     private val webArgs: WebArgs = WebArgs(savedStateHandle)
+     val webUrl = webArgs.url
 }
