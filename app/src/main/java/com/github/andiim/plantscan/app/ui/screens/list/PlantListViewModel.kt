35d58dde5114a14b75b81434179524dcdfb5fe77
Andi-IM <andi.irhamm@gmail.com>
2023-09-06 20:28:35 +0700

    refactor finished, remove obsolete viewmodel

--- app/src/main/java/com/github/andiim/plantscan/app/ui/screens/list/PlantListViewModel.kt
+++ app/src/main/java/com/github/andiim/plantscan/app/ui/screens/list/PlantListViewModel.kt
@@ -1,18 +1,16 @@
 package com.github.andiim.plantscan.app.ui.screens.list
 
+import androidx.lifecycle.ViewModel
 import androidx.paging.Pager
-import com.github.andiim.plantscan.app.PlantScanViewModel
 import com.github.andiim.plantscan.app.core.data.PlantRepositoryImpl
 import com.github.andiim.plantscan.app.core.domain.usecase.PlantUseCase
-import com.github.andiim.plantscan.app.core.domain.usecase.firebase_services.LogService
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class PlantListViewModel
 @Inject
-constructor(private val useCase: PlantUseCase, logService: LogService) :
-    PlantScanViewModel(logService) {
+constructor(private val useCase: PlantUseCase) : ViewModel() {
     val items =
         Pager(
             config = PlantRepositoryImpl.getDefaultPageConfig(),
